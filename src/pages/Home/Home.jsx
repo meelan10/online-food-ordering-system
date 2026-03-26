13a5b3fbab95c2f72d7c974a7f641638d3712703
meelan10 <milanwagle73@gmail.com>
2026-03-26 23:01:10 +0545

    menu added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css'
 import Header from '../../components/Navbar/Header/Header'
+import ExploreMenu from '../../components/Navbar/ExploreMenu/ExploreMenu'
 const Home = () => {
   return (
     <div>
       <Header />
+      <ExploreMenu />
     </div>
   )
 }
