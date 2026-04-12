a409f2789300d19870a10f990e1668ab8533e5b1
meelan10 <milanwagle73@gmail.com>
2026-04-12 21:48:03 +0545

    updated code

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -39,6 +39,37 @@ const Cart = () => {
       })}
       
     </div>
+    <div className="cart-bottom">
+      <div className="cart-total">
+        <h2>Cart Totals</h2>
+        <div>
+          <div className="cart-total-details">
+            <p className="">Subtotal</p>
+            <p className="">{0}</p>
+          </div>
+          <hr />
+          <div className="cart-total-details">
+            <p>Delivery food</p>
+            <p>{2}</p>
+          </div>
+          <hr />
+          <div className="cart-total-details">
+            <b>Total</b>
+            <b>{0}</b>
+          </div>
+        </div>
+          <button>Proceed to Checkout</button>
+      </div>
+      <div className="cart-promocode">
+        <div>
+          <p>If you have a promo code, Enter it here</p>
+          <div cart-promocode-input>
+        <input type='text' placeholder='promo code' />
+        <button>Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
 
   )
